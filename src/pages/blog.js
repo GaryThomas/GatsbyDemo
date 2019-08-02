d2a39b96e1181a181b8d90e13f5332fe0531d293
Gary Thomas <gary@mlbassoc.com>
2019-08-02 15:31:53 +0200

    Using GraphQL, set up to generate unique pages and links for each blog post - post template still needs filling out.  Stopped at 2:52:00

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,9 +1,9 @@
 import React from "react"
 import Layout from "../components/layout"
-import { graphql, useStaticQuery } from "gatsby"
+import { Link, graphql, useStaticQuery } from "gatsby"
 
 const BlogPage = () => {
-    const data = useStaticQuery(graphql`
+  const data = useStaticQuery(graphql`
     query {
         allMarkdownRemark {
           edges {
@@ -12,27 +12,33 @@ const BlogPage = () => {
                 title
                 date
               }
+              fields {
+                slug
+              }
             }
           }
         }
       }
     `)
-    // console.log(data)
-    return (
-        <Layout>
-            <h1>Blog?</h1>
-            <ol>
-                {data.allMarkdownRemark.edges.map((edge) => {
-                    return (
-                        <li>
-                            <h2>{edge.node.frontmatter.title}</h2>
-                            <p>{edge.node.frontmatter.date}</p>
-                        </li>
-                    )
-                })}
-            </ol>
-        </Layout>
-    )
+  // const linkName = `/blog/${edge.node.fields.slug}`
+  // console.log(data)
+  return (
+    <Layout>
+      <h1>Blog?</h1>
+      <ol>
+        {data.allMarkdownRemark.edges.map((edge) => {
+          return (
+            <li>
+              <Link to={`/blog/${edge.node.fields.slug}`}>
+                <h2>{edge.node.frontmatter.title}</h2>
+              </Link>
+              <p>{edge.node.frontmatter.date}</p>
+            </li>
+          )
+        })}
+      </ol>
+    </Layout>
+  )
 }
 
 export default BlogPage
---
+++ src/templates/blog-template.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../components/layout'
+
+const BlogTemplate = () => {
+    return (
+        <Layout>
+            <p>Something goes here</p>
+        </Layout>
+    )
+}
+
+export default BlogTemplate
