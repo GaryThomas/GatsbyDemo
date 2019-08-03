ecf8db535ba7e64079565b56e8b3ab5cc085c56b
Gary Thomas <gary@mlbassoc.com>
2019-08-03 17:30:13 +0200

    Fetch data from contentful - rendering actual rich text is pretty complex, especially embedded images (and who knows what else wasn't mentioned)

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,43 +1,27 @@
 const path = require('path')
 
-module.exports.onCreateNode = ({ node, actions }) => {
-    const { createNodeField } = actions
-    if (node.internal.type == "MarkdownRemark") {
-        const slug = path.basename(node.fileAbsolutePath, '.md')
-        // console.log(JSON.stringify(node, undefined, 4))
-        console.log(slug)
-        createNodeField({
-            node,
-            name: 'slug',
-            value: slug
-        })
-    }
-}
-
 module.exports.createPages = async ({ graphql, actions }) => {
     const { createPage } = actions
     const blogTemplate = path.resolve('./src/templates/blog-template.js')
     const res = await graphql(`
         query {
-            allMarkdownRemark {
+            allContentfulBlogPost {
               edges {
                 node {
-                  fields {
-                    slug
+                  slug
                 }
+              }
             }
-          }
         }
-      }
     `)
     // Note: only get here once the GraphQL query is complete
-    res.data.allMarkdownRemark.edges.forEach((edge) => {
-        console.log('==== create ', edge.node.fields.slug)
+    res.data.allContentfulBlogPost.edges.forEach((edge) => {
+        // console.log('==== create ', edge.node.slug)
         createPage({
             component: blogTemplate,
-            path: `/blog/${edge.node.fields.slug}`,
+            path: `/blog/${edge.node.slug}`,
             context: {
-                slug: edge.node.fields.slug
+                slug: edge.node.slug
             }
         })
     });
